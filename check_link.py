7ebe65b692d1b202184d9e6a107981a56ed2605b
XaMaB <y.simeonovv@gmail.com>
2019-10-05 21:57:02 +0300

    def slackhook

--- check_link.py
+++ check_link.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-import time, signal, sys
+import time, signal, sys, json, requests
 
-iface = "eth0.25"
+#globVars
+iface = "eth0"
 interval = 3
+webhook_url = 'https://hooks.slack.com/services/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'
 
 def signal_handler(sig, frame):
         print('Stoped!')
@@ -18,6 +20,27 @@ def get_bytes(line):
         data = f.read();
         return int(data)
 
+def slack_hook():
+    slack_data = {
+      "attachments": [
+        {
+          "text": str(rx_speed)+" Mbps",
+          "color": "#FF0000",
+          "mrkdwn_in": ["text"],
+          "fields": [
+        { "title": "Prober", "value": "MONIT_HOST", "short": "true" },
+          ]
+        }
+      ]
+    }
+
+    response = requests.post(
+        webhook_url, data=json.dumps(slack_data),
+        headers={'Content-Type': 'application/json'})
+    if response.status_code != 200:
+        raise ValueError(f'Request to slack returned an error {response.status_code}, the response is:{response.text}')
+
+
 while(True):
     tx1 = get_bytes('tx')
     rx1 = get_bytes('rx')
@@ -30,6 +53,7 @@ while(True):
     tx_speed = round((((tx2 - tx1) * 8) / 1000000) / interval, 2)
     rx_speed = round((((rx2 - rx1) * 8) / 1000000) / interval, 2)
 
-    print(f"RX: {rx_speed} Mbps | TX: {tx_speed} Mbps")
-
+    print(f'RX: {rx_speed} Mbps | TX: {tx_speed} Mbps')
+    if rx_speed > 10:
+        slack_hook()
 
